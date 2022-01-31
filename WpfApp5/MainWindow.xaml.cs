922d4898de98735044a89bc0345b298dda245504
Anton S <geodesist.sushkov@gmail.com>
2022-02-01 01:52:50 +0300

    команды в меню

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -46,17 +46,26 @@ namespace WpfApp5
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            textBox.FontWeight = FontWeights.Bold;
+            if (textBox.FontWeight == FontWeights.Normal)
+                textBox.FontWeight = FontWeights.Bold;
+            else
+                textBox.FontWeight = FontWeights.Normal;
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            textBox.FontStyle = FontStyles.Italic;
+            if (textBox.FontStyle == FontStyles.Normal)
+                textBox.FontStyle = FontStyles.Italic;
+            else
+                textBox.FontStyle = FontStyles.Normal;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            textBox.TextDecorations = TextDecorations.Underline;
+            if (textBox.TextDecorations.Count == 0)
+                textBox.TextDecorations.Add(TextDecorations.Underline);
+            else
+                textBox.TextDecorations.RemoveAt(0);
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
@@ -75,7 +84,13 @@ namespace WpfApp5
             }
         }
 
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
+
+        private void ExitExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Текст (*.txt)|*.txt|Все файлы (*.*)|*.*";
@@ -85,7 +100,7 @@ namespace WpfApp5
             }
         }
 
-        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        private void SaveExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "Текст (*.txt)|*.txt|Все файлы (*.*)|*.*";
@@ -93,11 +108,6 @@ namespace WpfApp5
             {
                 File.WriteAllText(saveFileDialog.FileName, textBox.Text);
             }
-        }
-
-        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
         }
     }
 }
