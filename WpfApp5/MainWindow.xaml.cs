dfee90cf47059a35f2db5b19b0262c1fee9709c4
Anton S <geodesist.sushkov@gmail.com>
2022-02-01 23:39:45 +0300

    task 8 added

--- WpfApp5/MainWindow.xaml.cs
+++ WpfApp5/MainWindow.xaml.cs
@@ -26,14 +26,14 @@ namespace WpfApp5
         {
             InitializeComponent();
         }
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
-            if (textBox != null)
-            {
-                textBox.FontFamily = new FontFamily(fontName);
-            }
-        }
+        //private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+        //    if (textBox != null)
+        //    {
+        //        textBox.FontFamily = new FontFamily(fontName);
+        //    }
+        //}
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
